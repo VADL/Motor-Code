2eb87b92a9ae9e4686479d6d806066361933e6b7
root <root@beaglebone.localdomain>
2016-05-21 17:35:08 -0500

    channel A test

--- Xavier/pwm/pwmA.cpp
+++ Xavier/pwm/pwmA.cpp
@@ -10,12 +10,12 @@ int main(int argc, char *argv[])
 {
         rc_initialize();
         int freq = 15*1000;
-        rc_set_pinmux_mode(3,PINMUX_PWM);
+        rc_set_pinmux_mode(2,PINMUX_PWM);
         rc_pwm_init(0,freq);
         float entry = 0.0;
  //       int encoder = 0;
         while(entry != -1.0){
-                rc_pwm_set_duty(0,'B', entry);
+                rc_pwm_set_duty(0,'A', entry);
                 printf("Enter a decimal b/w 0 and 1: ");
                 scanf("%f", &entry);
                 printf("You entered: %f\n", entry);
