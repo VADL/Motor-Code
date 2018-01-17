5de3b30b8681cee2da4967c130404c4ff403ce0a
Xavier <31713676+xaiblaze@users.noreply.github.com>
2018-01-16 19:55:12 -0600

    added encoder

--- Xavier/pwm/pwm.cpp
+++ Xavier/pwm/pwm.cpp
@@ -13,11 +13,14 @@ int main(int argc, char *argv[])
         rc_set_pinmux_mode(3,PINMUX_PWM);
         rc_pwm_init(0,freq);
         float entry = 0.0;
+        int encoder = 0;
         while(entry != -1.0){
                 rc_pwm_set_duty(0,'B', entry);
                 printf("Enter a decimal b/w 0 and 1: ");
                 scanf("%f", &entry);
                 printf("You entered: %f\n", entry);
+                enc = rc_get_encoder_pos(3);
+                printf("Enocoder value: %i\n", enc);
                 fflush(stdin);
         }
         rc_pwm_close(0);
